f29c65a748ed520cc5a2a7f8e854a0eab6d276eb
erictc95 <maxerictc95@hotmail.com>
2026-04-28 13:18:36 +0200

    Updated options blocked.

--- src/main/java/level1/exercise1/MenuBuilder.java
+++ src/main/java/level1/exercise1/MenuBuilder.java
@@ -1,6 +1,6 @@
 package level1.exercise1;
 
-public class MenuBuilder implements StartStep, IncomingStep, MainCourseStep, FinalStep {
+public class MenuBuilder implements StartStep {
 
     private Menu menu;
     private Dish currentDish;
@@ -10,52 +10,11 @@ public class MenuBuilder implements StartStep, IncomingStep, MainCourseStep, Fin
     }
 
     @Override
-    public MenuBuilder isVegan() {
-        currentDish.setVegan(true);
-        return this;
-    }
-
-    @Override
-    public MenuBuilder isGlutenFree() {
-        currentDish.setGlutenFree(true);
-        return this;
-    }
-
-    @Override
-    public MainCourseStep withSupplement(String supplement) {
-        currentDish.setSupplement(supplement);
-        return this;
-    }
-
-    @Override
-    public FinalStep withDessert(String dessert) {
-        menu.setDessert(dessert);
-        return this;
-    }
-
-    @Override
-    public FinalStep withCoffee(String coffee) {
-        menu.setCoffee(coffee);
-        return this;
-    }
-
-    @Override
-    public FinalStep withDrink(String drink) {
-        menu.setDrink(drink);
-        return this;
-    }
-
-    @Override
-    public Menu build() {
-        return menu;
-    }
-
-    @Override
     public IncomingStep withIncoming(String name) {
         Dish incoming = new Dish(name);
         menu.setIncoming(incoming);
         currentDish = incoming;
-        return this;
+        return new IncomingBuilder();
     }
 
     @Override
@@ -63,6 +22,89 @@ public class MenuBuilder implements StartStep, IncomingStep, MainCourseStep, Fin
         Dish mainCourse = new Dish(name);
         menu.setMainCourse(mainCourse);
         currentDish = mainCourse;
-        return this;
+        return new MainCourseBuilder();
     }
+
+    private class IncomingBuilder implements IncomingStep {
+
+        @Override
+        public IncomingStep isVegan() {
+            currentDish.setVegan(true);
+            return this;
+        }
+
+        @Override
+        public IncomingStep isGlutenFree() {
+            currentDish.setGlutenFree(true);
+            return this;
+        }
+
+         @Override
+        public MainCourseStep withMainCourse(String name) {
+            Dish mainCourse = new Dish(name);
+            menu.setMainCourse(mainCourse);
+            currentDish = mainCourse;
+            return new MainCourseBuilder();
+         }
+    }
+
+    private class MainCourseBuilder implements MainCourseStep {
+
+        @Override
+        public MainCourseStep isVegan() {
+            currentDish.setVegan(true);
+            return this;
+        }
+
+        @Override
+        public MainCourseStep isGlutenFree() {
+            currentDish.setGlutenFree(true);
+            return this;
+        }
+
+        @Override
+        public MainCourseStep withSupplement(String supplement) {
+            currentDish.setSupplement(supplement);
+            return this;
+        }
+
+        @Override
+        public FinalStep withDessert(String dessert) {
+            menu.setDessert(dessert);
+            return new FinalBuilder();
+        }
+
+        @Override
+        public FinalStep withCoffee(String coffee) {
+            menu.setCoffee(coffee);
+            return new FinalBuilder();
+        }
+
+        @Override
+        public FinalStep withDrink(String drink) {
+            menu.setDrink(drink);
+            return new FinalBuilder();
+        }
+
+        @Override
+        public Menu build() {
+            return menu;
+        }
+    }
+
+    private class FinalBuilder implements FinalStep {
+
+        @Override
+        public FinalStep withDrink(String drink) {
+            menu.setDrink(drink);
+            return this;
+        }
+
+        @Override
+        public Menu build() {
+            return menu;
+        }
+    }
+
+
 }
